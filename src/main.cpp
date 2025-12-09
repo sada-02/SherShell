b842e81ae07f625ce2668327de61423b7ce097bc
sada-02 <kartiksarda.2005@gmail.com>
2025-12-09 23:01:57 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,61 +2,95 @@
 #include <vector>
 #include <string>
 #include <map>
-#include <sys/stat.h>
 #include <sstream>
-#include <unistd.h>
+#include <filesystem>
 using namespace std;
+namespace fs = filesystem;
 
 map<string,string> commands;
+vector<fs::path> execFiles;
+vector<string> builtins = {"echo" , "exit" , "type" , "exit"};
+string PATH;
+
+vector<string> tokenize(string& query) {
+  stringstream q(query);
+  string temp;
+  vector<string> tokens;
+
+  while(getline(q,temp,' ')) {
+    tokens.emplace_back(temp);
+  }
+
+  return tokens;
+}
+
+void getExecFiles() {
+  int start=0;
+  for(int i=0 ;i<PATH.size() ;i++) {
+    if(PATH[i] == ':') {
+      string p = PATH.substr(start , i-start);
+
+      if(fs::exists(p)) {
+        for(const auto& DIR : fs::directory_iterator(p)) {
+          if(!DIR.is_directory() && (DIR.status().permissions() & fs::perms::owner_exec)==fs::perms::owner_exec) {
+            execFiles.emplace_back(DIR.path());
+          }
+        }
+      }
+
+      start = i+1;
+    }
+  }
+}
+
+fs::path findPath(string& s) {
+  for(const auto& p :execFiles) {
+    if(p.stem() == s) {
+      return p;
+    }
+  }
+
+  return fs::path{};
+}
 
 int main() {
   cout<<unitbuf;
   cerr<<unitbuf;
 
-  commands["echo"] = "sh";
-  commands["exit"] = "sh";
-  commands["type"] = "sh";
-
-  string PATH = getenv("PATH");
+  for(string& b : builtins) commands[b] = "sh";
+  PATH = getenv("PATH");
+  PATH.append(":");
+  getExecFiles();
 
   while(true) {
     cout << "$ ";
     string cmd;
     getline(cin,cmd);
 
-    if(cmd == "exit") {
+    vector<string> tokens = tokenize(cmd);
+
+    if(tokens[0] == "exit") {
       break;
     }
-    else if(cmd.substr(0,5) == "echo ") {
-      cout<<cmd.substr(5)<<endl;
+    else if(tokens[0] == "echo") {
+      for(string& str : tokens) {
+        cout<<str<<" "<<endl;
+      }
     }
-    else if(cmd.substr(0,5) == "type ") {
+    else if(tokens[0] == "type") {
       string leftOver = cmd.substr(5);
 
       if(commands[leftOver] == "sh") {
         cout<<leftOver<<" is a shell builtin"<<endl;
       }
       else {
-        string dir;
-        stringstream path(PATH);
-        bool flag = false;
-
-        while(getline(path , dir , ':')) {
-          string fullPath = dir + "/" + leftOver;
-
-          struct stat sb;
-          if(stat(fullPath.c_str() , &sb) == 0) {
-            if(access(fullPath.c_str() , X_OK) == 0) {
-              cout<<leftOver<<" is "<<fullPath<<endl;
-              flag = true;
-            }
-          }
-
-          if(flag) break;
+        auto p = findPath(leftOver);
+        if(!p.empty()) {
+          cout<<leftOver<<" is "<<p.string()<<endl;
         }
-        if(!flag)
-        
-        cout<<leftOver<<": not found"<<endl;
+        else {
+          cout<<leftOver<<": not found"<<endl;
+        } 
       }
     }
     else {
