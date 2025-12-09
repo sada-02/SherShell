d7f8634d105e35758efefaf41777fb0353b064d2
sada-02 <kartiksarda.2005@gmail.com>
2025-12-09 23:44:13 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -105,7 +105,7 @@ int main() {
         pid_t pid = fork();
 
         if(pid == 0) {
-          execvp(isExec , args.data());
+          execvp(isExec.string() , args.data());
         }
         else {
           int status;
