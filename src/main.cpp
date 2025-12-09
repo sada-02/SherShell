d92d38889a90e75b9c4d414f1dfc668532e798b6
sada-02 <kartiksarda.2005@gmail.com>
2025-12-09 21:58:16 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <sys/stat.h>
+#include <sstream>
+#include <unistd.h>
 using namespace std;
 
 map<string,string> commands;
@@ -13,6 +16,8 @@ int main() {
   commands["echo"] = "sh";
   commands["exit"] = "sh";
   commands["type"] = "sh";
+
+  string PATH = getenv("PATH");
 
   while(true) {
     cout << "$ ";
@@ -27,10 +32,30 @@ int main() {
     }
     else if(cmd.substr(0,5) == "type ") {
       string leftOver = cmd.substr(5);
+
       if(commands[leftOver] == "sh") {
         cout<<leftOver<<" is a shell builtin"<<endl;
       }
       else {
+        string dir;
+        stringstream path(PATH);
+        bool flag = false;
+
+        while(getline(path , dir , ':')) {
+          string fullPath = dir + "/" + leftOver;
+
+          struct stat sb;
+          if(stat(fullPath.c_str() , &sb) == 0) {
+            if(access(fullPath.c_str() , X_OK) == 0) {
+              cout<<leftOver<<" is "<<fullPath<<endl;
+              flag = true;
+            }
+          }
+
+          if(flag) break;
+        }
+        if(!flag)
+        
         cout<<leftOver<<": not found"<<endl;
       }
     }
