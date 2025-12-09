2fae539ab25675806a52a2971ff9682abcf12eca
sada-02 <kartiksarda.2005@gmail.com>
2025-12-09 19:58:40 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <map>
 using namespace std;
+
+map<string,string> commands;
 
 int main() {
   cout<<unitbuf;
   cerr<<unitbuf;
+
+  commands["echo"] = "sh";
+  commands["exit"] = "sh";
+  commands["type"] = "sh";
 
   while(true) {
     cout << "$ ";
@@ -14,8 +22,17 @@ int main() {
     if(cmd == "exit") {
       break;
     }
-    else if(cmd.substr(0,4) == "echo" && cmd[4] == ' ') {
+    else if(cmd.substr(0,5) == "echo ") {
       cout<<cmd.substr(5)<<endl;
+    }
+    else if(cmd.substr(0,5) == "type ") {
+      string leftOver = cmd.substr(5);
+      if(commands[leftOver] == "sh") {
+        cout<<leftOver<<" is a shell builtin"<<endl;
+      }
+      else {
+        cout<<leftOver<<": not found"<<endl;
+      }
     }
     else {
       cout<<cmd<<": command not found"<<endl; 
