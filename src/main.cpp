e712053bebe7a56da633a2bca4a3d3f8fb77b5ae
sada-02 <kartiksarda.2005@gmail.com>
2025-12-11 15:28:27 +0530

    autocompletion with lcp

--- src/main.cpp
+++ src/main.cpp
@@ -292,8 +292,8 @@ vector<string> findExecWith(const string& str) {
 }
 
 string longestCommonPrefix(vector<string>& words) {
-    string lcp = words[0];
-    for(int i=1 ;i<words.size() ;i++) {
+    string lcp = words[words.size()-1];
+    for(int i=1 ;i<words.size()-1 ;i++) {
       for(int j=0 ;j<lcp.size() ;j++) {
         if(words[i][j] != lcp[j]) {
           lcp = lcp.substr(0,j+1);
