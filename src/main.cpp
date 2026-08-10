f990c19f770fcf71e4e8ceb99f8904ba611a1376
sada-02 <kartiksarda.2005@gmail.com>
2026-08-10 13:28:33 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -721,6 +721,11 @@ void iter(string& cmd, bool inPipeline = false) {
   }
   else if(tokens[0] == "cat") {
     for(int i=1 ;i<maxIDX ;i++) {
+      if(!tokens[i].empty()) {
+        while(!tokens[i].empty() && tokens[i].back() == '\t') {
+          tokens[i].pop_back();
+        }
+      }
       tokens[i] = checkAutoCompletion->startWith(tokens[i])[0];
       if(!fs::exists(fs::path(tokens[i]))) {
         errorstr += "cat: " + tokens[i] + ": No such file or directory" + '\n';
